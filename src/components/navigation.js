e6b85ccc0633b07969ed862e19b2f1b7f000c721
Juan Macias <Juan.Macias@sa.catapult.org.uk>
2020-05-10 12:13:26 +0100

    Adding prettier

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,18 +1,25 @@
-import React from "react"
-import {Link} from "gatsby"
-import ThemeChanger from "../components/themeChanger"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React from 'react'
+import { Link } from 'gatsby'
+import ThemeChanger from '../components/themeChanger'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default (props) => (
-  <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
-    <a href="https://github.com/Juanezm" target="_blank" rel="noopener noreferrer">
-      <FontAwesomeIcon icon={['fab', 'github']} />
-    </a>
-    <a href="https://www.linkedin.com/in/juanezm/" target="_blank" rel="noopener noreferrer">
-      <FontAwesomeIcon icon={['fab', 'linkedin']} />
-    </a>
-    <ThemeChanger/>
-  </nav>
-  
+    <nav className="navigation">
+        <Link to="/contact">Contact</Link>
+        <a
+            href="https://github.com/Juanezm"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <FontAwesomeIcon icon={['fab', 'github']} />
+        </a>
+        <a
+            href="https://www.linkedin.com/in/juanezm/"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <FontAwesomeIcon icon={['fab', 'linkedin']} />
+        </a>
+        <ThemeChanger />
+    </nav>
 )
